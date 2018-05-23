e6a2a114b5b4575abccfbf0d918106d765a98f49
yevheniia <evgesha.drozdova@gmail.com>
2018-05-23 09:24:41 +0300

    fonts

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,40 @@
-(function() {
-    $(document).ready(function(){
-        $(".small-pics img.slide-img").on("click", function() {
-            var carousel_selector = $(this).parent().attr("data-carousel");
-            $(carousel_selector).carousel($(this).index());
-        });    
+var modal = $('#myModal');
+var modalImg = $("#img01");
+
+// Get the modal
+$('img').click(function() {
+    var theClass = this.className;
+    var imgArray = document.getElementsByClassName(theClass);
+    var index = $(imgArray).index(this);
+    modalImg.attr("src", this.src);
+    modal.click(function() {
+        modalImg.attr("src", this.src);
     });
-})();
+
+
+    $('#next').click(function() {
+        index = index + 1;
+        if(index < imgArray.length) {
+            modalImg.attr("src", imgArray[index].src);
+        }
+
+
+        if(index === imgArray.length) {
+            index = 0;
+            modalImg.attr("src", imgArray[index].src);
+        }
+
+
+    });
+
+
+    $('#myModal').css("display","block");
+
+});
+
+var span = $("#close");
+
+// When the user clicks on <span> (x), close the modal
+$(".close").click(function() {
+    $('#myModal').css("display","none");
+})
